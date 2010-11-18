2cd38681587acafb6f7d9e8056a14fdbcb9aa743
Max Reichardt <reichardt@informatik.uni-kl.de>
2010-11-18 22:37:17 +0000

    Added test program that displays finroc configuration options

---
+++ test/tPrintConfig.cpp
@@ -0,0 +1,52 @@
+/**
+ * You received this file as part of an advanced experimental
+ * robotics framework prototype ('finroc')
+ *
+ * Copyright (C) 2010 Max Reichardt,
+ *   Robotics Research Lab, University of Kaiserslautern
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#include "core/tRuntimeEnvironment.h"
+#include "core/port/std/tPortDataManager.h"
+#include "core/datatype/tCoreNumber.h"
+#include "core/datatype/tCoreString.h"
+
+int main(int argc__, char **argv__)
+{
+  printf("Finroc Configuration:\n");
+
+#ifdef __JC_BASIC_REUSABLE_TRACING_ENABLED__
+  printf(" __JC_BASIC_REUSABLE_TRACING_ENABLED__: true\n");
+#else
+  printf(" __JC_BASIC_REUSABLE_TRACING_ENABLED__: false\n");
+#endif
+
+#ifdef __JC_DETAILED_REUSABLE_TRACING_ENABLED__
+  printf(" __JC_DETAILED_REUSABLE_TRACING_ENABLED__: true\n");
+#else
+  printf(" __JC_DETAILED_REUSABLE_TRACING_ENABLED__: false\n");
+#endif
+
+#ifdef __JC_ENFORCE_LOCK_ORDER__
+  printf(" __JC_ENFORCE_LOCK_ORDER__: true\n");
+#else
+  printf(" __JC_ENFORCE_LOCK_ORDER__: false\n");
+#endif
+
+  printf(" sizeof(tPortDataManager): %d\n", sizeof(finroc::core::tPortDataManager));
+  printf(" sizeof(tCoreNumber): %d\n", sizeof(finroc::core::tCoreNumber));
+  printf(" sizeof(tCoreString): %d\n", sizeof(finroc::core::tCoreString));
+}
