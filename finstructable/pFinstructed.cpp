06b055d76ef34d9473dfcdffb2bac4fdd781579d
Max Reichardt <mreichardt@gmx.net>
2011-09-06 17:39:06 +0200

    Made pFinstructed use default_main_wrapper.

--- finstructable/pFinstructed.cpp
+++ finstructable/pFinstructed.cpp
@@ -2,7 +2,7 @@
  * You received this file as part of an advanced experimental
  * robotics framework prototype ('finroc')
  *
- * Copyright (C) 2010 Max Reichardt,
+ * Copyright (C) 2010-2011 Max Reichardt,
  *   Robotics Research Lab, University of Kaiserslautern
  *
  * This program is free software; you can redistribute it and/or
@@ -19,45 +19,81 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-#include "core/finstructable/pFinstructed.h"
-#include "core/tRuntimeEnvironment.h"
-#include "plugins/tcp/tTCPServer.h"
+
+#include "core/default_main_wrapper.h"
 #include "core/finstructable/tFinstructableGroup.h"
 
-namespace finroc
-{
-namespace core
-{
-void pFinstructed::Main(::finroc::util::tArrayWrapper<util::tString>& args)
-{
-  tRuntimeEnvironment* re = tRuntimeEnvironment::GetInstance();
-  tcp::tTCPServer* server = new tcp::tTCPServer(4444, true, NULL);
-  server->Init();
+//----------------------------------------------------------------------
+// Namespace usage
+//----------------------------------------------------------------------
+using namespace finroc::core;
 
-  tFinstructableGroup* fg = new tFinstructableGroup(re, "finstructed", "etc/finstructed_test.xml");
-  fg->Init();
+//----------------------------------------------------------------------
+// Forward declarations / typedefs / enums
+//----------------------------------------------------------------------
 
-  while (true)
+//----------------------------------------------------------------------
+// Const values
+//----------------------------------------------------------------------
+const char * const cPROGRAM_VERSION = "ver 1.0";
+const char * const cPROGRAM_DESCRIPTION = "This program instantiates and executes all .xml files passed as command line arguments.";
+
+//----------------------------------------------------------------------
+// Implementation
+//----------------------------------------------------------------------
+
+//----------------------------------------------------------------------
+// StartUp
+//----------------------------------------------------------------------
+void StartUp()
+{
+
+}
+
+//----------------------------------------------------------------------
+// InitMainGroup
+//----------------------------------------------------------------------
+void InitMainGroup(finroc::core::tThreadContainer *main_thread, std::vector<char*> remaining_args)
+{
+
+  // slightly ugly command line parsing
+  for (size_t i = 0; i < remaining_args.size(); i++)
   {
-    try
+    finroc::util::tString arg(remaining_args[i]);
+    if (arg.Substring(arg.Length() - 4).Equals(".xml"))
     {
-      util::tThread::Sleep(10000);
+      finroc::util::tString group_name;
+      finroc::util::tString xml_name;
+      if (arg.Contains(":"))
+      {
+        std::vector<finroc::util::tString> split = arg.Split(":");
+        if (split.size() != 2)
+        {
+          continue;
+        }
+        group_name = split[0];
+        xml_name = split[1];
+      }
+      else
+      {
+        xml_name = arg;
+        if (arg.Contains("/"))
+        {
+          group_name = arg.Substring(arg.LastIndexOf("/") + 1); // cut off path
+        }
+        group_name = group_name.Substring(0, group_name.Length() - 4); // cut off .xml
+      }
+
+      new tFinstructableGroup(main_thread, group_name, xml_name);
     }
-    catch (const util::tInterruptedException& e)
-    {
-    }
+  }
+
+  if (main_thread->ChildCount() == 0)
+  {
+    FINROC_LOG_PRINT_STATIC(rrlib::logging::eLL_USER, "No finstructable groups specified.");
+    FINROC_LOG_PRINT_STATIC(rrlib::logging::eLL_USER, "Usage:  finstructed <xml-file1> <xml-file2>");
+    FINROC_LOG_PRINT_STATIC(rrlib::logging::eLL_USER, "To set group name use <name>:<xml-file>. Otherwise xml-file name is used as group name.");
+    exit(-1);
   }
 }
 
-} // namespace finroc
-} // namespace core
-
-int main(int argc__, char **argv__)
-{
-  ::finroc::util::tArrayWrapper< ::finroc::util::tString> sa(argc__ <= 0 ? 0 : (argc__ - 1));
-  for (int i = 1; i < argc__; i++)
-  {
-    sa[i - 1] = ::finroc::util::tString(argv__[i]);
-  }
-  ::finroc::core::pFinstructed::Main(sa);
-}
