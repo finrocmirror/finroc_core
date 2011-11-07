4da4e3f61cd592635874cdc29a00d529d11dd552
Max Reichardt <mreichardt@gmx.net>
2011-11-07 01:19:25 +0100

    Added tPortGroup source files.

---
+++ port/tPortGroup.cpp
@@ -0,0 +1,163 @@
+/**
+ * You received this file as part of an advanced experimental
+ * robotics framework prototype ('finroc')
+ *
+ * Copyright (C) 2011 Max Reichardt,
+ *   Robotics Research Lab, University of Kaiserslautern
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+#include "core/port/tPortGroup.h"
+#include "core/port/tAbstractPort.h"
+#include "core/port/std/tPortBase.h"
+#include "core/port/cc/tCCPortBase.h"
+#include "core/port/rpc/tInterfacePort.h"
+
+namespace finroc
+{
+namespace core
+{
+
+tPortGroup::tPortGroup(tFrameworkElement* parent, const util::tString& name, int flags, int default_port_flags) :
+    tEdgeAggregator(parent, name, flags),
+    default_port_flags(default_port_flags)
+{
+}
+
+void tPortGroup::ConnectImpl(int op, tPortGroup* group, const util::tString& group_link, bool create_missing_ports, tAbstractPort* start_with, int count, const util::tString& port_prefix, const util::tString& other_port_prefix)
+{
+  int org_count = count;
+  tChildIterator ci(this);
+  tAbstractPort* p = NULL;
+  while ((p = ci.NextPort()) != NULL)
+  {
+    util::tString name(p->GetDescription());
+    if (p == start_with)
+    {
+      start_with = NULL;
+    }
+    if (start_with != NULL || (!name.StartsWith(port_prefix)))
+    {
+      continue;
+    }
+    if (count == 0)
+    {
+      return;
+    }
+    count--;
+    name = name.Substring(port_prefix.Length());
+
+    // connect-function specific part
+    if (op <= 1)
+    {
+      tFrameworkElement* child = group->GetChild(other_port_prefix + name);
+      if (child != NULL && child->IsPort())
+      {
+        if (op == 0)
+        {
+          p->ConnectToSource(static_cast<tAbstractPort*>(child));
+        }
+        else
+        {
+          p->ConnectToTarget(static_cast<tAbstractPort*>(child));
+        }
+      }
+      else if (create_missing_ports)
+      {
+        child = group->CreatePort(other_port_prefix + name, p->GetDataType(), 0);
+        if (op == 0)
+        {
+          p->ConnectToSource(static_cast<tAbstractPort*>(child));
+        }
+        else
+        {
+          p->ConnectToTarget(static_cast<tAbstractPort*>(child));
+        }
+      }
+    }
+    else if (op == 2)
+    {
+      p->ConnectToSource(group_link + "/" + other_port_prefix + name);
+    }
+    else if (op == 3)
+    {
+      p->ConnectToTarget(group_link + "/" + other_port_prefix + name);
+    }
+    // connect-function specific part end
+
+  }
+  if (start_with != NULL)
+  {
+    FINROC_LOG_PRINT(rrlib::logging::eLL_WARNING, log_domain, "Port ", start_with->GetQualifiedName(), " no child of ", this->GetQualifiedName(), ". Did not connect anything.");
+  }
+  if (count > 0)
+  {
+    FINROC_LOG_PRINT(rrlib::logging::eLL_WARNING, log_domain, "Could only connect ", (org_count - count), " ports (", org_count, " desired).");
+  }
+}
+
+tAbstractPort* tPortGroup::CreatePort(const util::tString& name, rrlib::serialization::tDataTypeBase type, int extra_flags)
+{
+  FINROC_LOG_PRINT(rrlib::logging::eLL_DEBUG_VERBOSE_1, log_domain, "Creating port ", name, " in IOVector ", this->GetQualifiedLink());
+  tAbstractPort* ap = NULL;
+  if (tFinrocTypeInfo::IsStdType(type))
+  {
+    ap = new tPortBase(tPortCreationInfoBase(name, this, type, default_port_flags | extra_flags));
+  }
+  else if (tFinrocTypeInfo::IsCCType(type))
+  {
+    ap = new tCCPortBase(tPortCreationInfoBase(name, this, type, default_port_flags | extra_flags));
+  }
+  else if (tFinrocTypeInfo::IsMethodType(type))
+  {
+    ap = new tInterfacePort(name, this, type, tInterfacePort::eRouting);
+  }
+  else
+  {
+    FINROC_LOG_PRINT(rrlib::logging::eLL_WARNING, log_domain, "Cannot create port with type: ", type.GetName());
+  }
+  if (ap != NULL)
+  {
+    ap->Init();
+  }
+  return ap;
+}
+
+void tPortGroup::DisconnectAll(bool incoming, bool outgoing, tAbstractPort* start_with, int count)
+{
+  tChildIterator ci(this);
+  tAbstractPort* p = NULL;
+  while ((p = ci.NextPort()) != NULL)
+  {
+    if (p == start_with)
+    {
+      start_with = NULL;
+    }
+    if (start_with != NULL)
+    {
+      continue;
+    }
+    if (count == 0)
+    {
+      return;
+    }
+    count--;
+    p->DisconnectAll(incoming, outgoing);
+  }
+}
+
+} // namespace finroc
+} // namespace core
+
---
+++ port/tPortGroup.h
@@ -0,0 +1,189 @@
+/**
+ * You received this file as part of an advanced experimental
+ * robotics framework prototype ('finroc')
+ *
+ * Copyright (C) 2011 Max Reichardt,
+ *   Robotics Research Lab, University of Kaiserslautern
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef core__port__tPortGroup_h__
+#define core__port__tPortGroup_h__
+
+#include "rrlib/finroc_core_utils/definitions.h"
+
+#include "core/port/tEdgeAggregator.h"
+#include "core/port/tPortWrapperBase.h"
+
+namespace finroc
+{
+namespace core
+{
+
+/*!
+ * \author Max Reichardt
+ *
+ * Group of ports.
+ *
+ * Sensor Inputs, Contoller Inputs etc. of a module are such groups.
+ *
+ * They can be used to conveniently connect whole groups of ports at once -
+ * for instance, by-name and possibly even create ports in target group
+ * if they do not exist yet.
+ *
+ * All convenience functions to connect groups of ports should be added
+ * to this class.
+ */
+class tPortGroup : public tEdgeAggregator
+{
+private:
+
+  /*! Default flags for any ports to be created in this Group */
+  const int default_port_flags;
+
+public:
+
+  /*!
+   * \param default_port_flags Default flags for any ports to be created in this Group
+   * (see FrameworkElement for other parameter description)
+   */
+  tPortGroup(tFrameworkElement* parent, const util::tString& name, int flags, int default_port_flags);
+
+  /*!
+   * Implementation of several Connect... functions (to avoid major code duplication)
+   *
+   * \param op Internal Opcode
+   * \param group Partner port group
+   * \param group_link Partner port group link (if port group is null)
+   * \param create_missing_ports Create ports in source, if this group has ports with names that cannot be found in source.
+   * \param start_with Port to start connecting with (NULL = first port)
+   * \param count Number of ports to connect - starting with start port (-1 = all ports)
+   * \param port_prefix Prefix of ports in this group. Prefix is cut off when comparing names. Ports without this prefix are skipped.
+   * \param other_port_prefix Prefix of ports in source group to ignore. This is prepended when ports are created.
+   */
+  void ConnectImpl(int op, tPortGroup* group, const util::tString& group_link, bool create_missing_ports, tAbstractPort* start_with = NULL, int count = -1, const util::tString& port_prefix = "", const util::tString& other_port_prefix = "");
+
+  /*!
+   * Connects all ports to ports with the same name in source port group.
+   *
+   * \param source source port group
+   * \param create_missing_ports Create ports in source, if this group has ports with names that cannot be found in source.
+   * \param start_with Port to start connecting with (NULL = first port)
+   * \param count Number of ports to connect - starting with start port (-1 = all ports)
+   * \param port_prefix Prefix of ports in this group. Prefix is cut off when comparing names. Ports without this prefix are skipped.
+   * \param source_port_prefix Prefix of ports in source group to ignore. This is prepended when ports are created.
+   */
+  void ConnectToSourceByName(tPortGroup& source, bool create_missing_ports, tAbstractPort* start_with = NULL, int count = -1, const util::tString& port_prefix = "", const util::tString& source_port_prefix = "")
+  {
+    ConnectImpl(0, &source, "", create_missing_ports, start_with, count, port_prefix, source_port_prefix);
+  }
+  template <typename T>
+  void ConnectToSourceByName(tPortGroup& source, bool create_missing_ports, tPortWrapperBase<T> start_with, int count = -1, const util::tString& port_prefix = "", const util::tString& source_port_prefix = "")
+  {
+    ConnectToSourceByName(source, create_missing_ports, start_with.GetWrapped(), count, port_prefix, source_port_prefix);
+  }
+
+  /*!
+   * Connects/links all ports to ports with the same name in source port group.
+   * (connection is (re)established when link is available)
+   *
+   * \param source_link Link name of source port group (relative to this port group)
+   * \param start_with Port to start connecting with (NULL = first port)
+   * \param count Number of ports to connect - starting with start port (-1 = all ports)
+   * \param port_prefix Prefix of ports in this group. Prefix is cut off when comparing names. Ports without this prefix are skipped.
+   * \param source_port_prefix Prefix of ports in source group to ignore. This is prepended when ports are created.
+   */
+  void ConnectToSourceByName(const util::tString& source_link, tAbstractPort* start_with = NULL, int count = -1, const util::tString& port_prefix = "", const util::tString& source_port_prefix = "")
+  {
+    ConnectImpl(2, NULL, source_link, false, start_with, count, port_prefix, source_port_prefix);
+  }
+  template <typename T>
+  void ConnectToSourceByName(const util::tString& source_link, tPortWrapperBase<T> start_with, int count = -1, const util::tString& port_prefix = "", const util::tString& source_port_prefix = "")
+  {
+    ConnectToSourceByName(source_link, start_with.GetWrapped(), count, port_prefix, source_port_prefix);
+  }
+
+  /*!
+   * Connects all ports to ports with the same name in target port group.
+   *
+   * \param target target port group
+   * \param create_missing_ports Create ports in target, if this group has ports with names that cannot be found in target.
+   * \param start_with Port to start connecting with (NULL = first port)
+   * \param count Number of ports to connect - starting with start port (-1 = all ports)
+   * \param port_prefix Prefix of ports in this group. Prefix is cut off when comparing names. Ports without this prefix are skipped.
+   * \param target_port_prefix Prefix of ports in target group to ignore. This is prepended when ports are created.
+   */
+  void ConnectToTargetByName(tPortGroup& target, bool create_missing_ports, tAbstractPort* start_with = NULL, int count = -1, const util::tString& port_prefix = "", const util::tString& target_port_prefix = "")
+  {
+    ConnectImpl(1, &target, "", create_missing_ports, start_with, count, port_prefix, target_port_prefix);
+  }
+  template <typename T>
+  void ConnectToTargetByName(tPortGroup& target, bool create_missing_ports, tPortWrapperBase<T> start_with, int count = -1, const util::tString& port_prefix = "", const util::tString& target_port_prefix = "")
+  {
+    ConnectToTargetByName(target, create_missing_ports, start_with.GetWrapped(), count, port_prefix, target_port_prefix);
+  }
+
+  /*!
+   * Connects/links all ports to ports with the same name in target port group.
+   * (connection is (re)established when link is available)
+   *
+   * \param target_link Link name of target port group (relative to this port group)
+   * \param start_with Port to start connecting with (NULL = first port)
+   * \param count Number of ports to connect - starting with start port (-1 = all ports)
+   * \param port_prefix Prefix of ports in this group. Prefix is cut off when comparing names. Ports without this prefix are skipped.
+   * \param target_port_prefix Prefix of ports in target group to ignore. This is prepended when ports are created.
+   */
+  void ConnectToTargetByName(const util::tString& target_link, tAbstractPort* start_with = NULL, int count = -1, const util::tString& port_prefix = "", const util::tString& target_port_prefix = "")
+  {
+    ConnectImpl(3, NULL, target_link, false, start_with, count, port_prefix, target_port_prefix);
+  }
+  template <typename T>
+  void ConnectToTargetByName(const util::tString& target_link, tPortWrapperBase<T> start_with, int count = -1, const util::tString& port_prefix = "", const util::tString& target_port_prefix = "")
+  {
+    ConnectToTargetByName(target_link, start_with.GetWrapped(), count, port_prefix, target_port_prefix);
+  }
+
+  /*!
+   * Create port in this group
+   *
+   * \param name Name of port
+   * \param type Data type of port
+   * \param extra_flags Any extra flags for port
+   * \return Created port
+   */
+  tAbstractPort* CreatePort(const util::tString& name, rrlib::serialization::tDataTypeBase type, int extra_flags = 0);
+
+  /*!
+   * Disconnect all of port group's ports
+   *
+   * \param incoming Disconnect incoming edges?
+   * \param outgoing Disconnect outgoing edges?
+   * \param start_with Port to start disconnecting with (NULL = first port)
+   * \param count Number of ports to disconnect - starting with start port (-1 = all ports)
+   */
+  void DisconnectAll(bool incoming = true, bool outgoing = true, tAbstractPort* start_with = NULL, int count = -1);
+  template <typename T>
+  void DisconnectAll(bool incoming, bool outgoing, tPortWrapperBase<T> start_with, int count = -1)
+  {
+    DisconnectAll(incoming, outgoing, start_with.GetWrapped());
+  }
+
+};
+
+} // namespace finroc
+} // namespace core
+
+#endif // core__port__tPortGroup_h__
