f5af77de14de7bd98672ac8944116e4e5ea331cd
Max Reichardt <mreichardt@gmx.net>
2013-02-07 12:04:56 +0100

    Typo in comment

--- internal/tFrameworkElementRegister.cpp
+++ internal/tFrameworkElementRegister.cpp
@@ -19,7 +19,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
 //----------------------------------------------------------------------
-/*!\file    core/internal/tFrameworkElementRegister2.cpp
+/*!\file    core/internal/tFrameworkElementRegister.cpp
  *
  * \author  Max Reichardt
  *
